d675ec482f79270a5761d3a7d7b332f84cfb150d
EugeneLiu <liuyichen@EugeneLiu.xyz>
2018-06-26 21:19:19 +0800

    ubuntu mate init shell
    change the sources.list of ubuntu mate and install vim

--- UbuntuMate.sh
+++ UbuntuMate.sh
@@ -1,13 +1,13 @@
-su
-cd /etc/apt
-rm sources.list
-echo "deb http://ports.ubuntu.com/ xenial main restricted universe multiverse
+rm /etc/apt/sources.list
+cat > /etc/apt/sources.list << END_TEXT
+deb http://ports.ubuntu.com/ xenial main restricted universe multiverse
 deb-src http://ports.ubuntu.com/ xenial main restricted universe multiverse
 deb http://ports.ubuntu.com/ xenial-updates main restricted universe multiverse
 deb-src http://ports.ubuntu.com/ xenial-updates main restricted universe multiverse
 deb http://ports.ubuntu.com/ xenial-security main restricted universe multiverse
 deb-src http://ports.ubuntu.com/ xenial-security main restricted universe multiverse
 deb http://ports.ubuntu.com/ xenial-backports main restricted universe multiverse
-deb-src http://ports.ubuntu.com/ xenial-backports main restricted universe multiverse" >> sources.list
+deb-src http://ports.ubuntu.com/ xenial-backports main restricted universe multiverse
+END_TEXT
 apt update && apt upgrade
 apt install vim -y
